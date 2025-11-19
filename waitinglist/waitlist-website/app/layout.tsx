baf93babc3571cd26e81888abd39215a17d4e455
VilemB <barnetv7@gmail.com>
2025-11-19 19:57:39 +0100

    favicon

--- waitinglist/waitlist-website/app/layout.tsx
+++ waitinglist/waitlist-website/app/layout.tsx
@@ -23,19 +23,28 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: '/favicon-16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/icon.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
         url: '/icon-light-32x32.png',
+        sizes: '32x32',
         media: '(prefers-color-scheme: light)',
       },
       {
         url: '/icon-dark-32x32.png',
+        sizes: '32x32',
         media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
       },
     ],
     apple: '/apple-icon.png',
+    shortcut: '/favicon-16.png',
   },
 }
 
